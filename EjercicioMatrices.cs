fca2cbcf0f86a5eada59e78e975f2ffd3495f521
Victor Aracil <victoraracil55@gmail.com>
2024-12-19 18:09:07 +0100

    Ejercicio de matrices acabado

--- EjercicioMatrices.cs
+++ EjercicioMatrices.cs
@@ -1,5 +1,5 @@
 using System;
-class ExamenVictorAracil{
+class EjercicioMatrices{
 	public struct structMatrices {
 		public int [,]m1;
 		public int [,]m2;
@@ -21,7 +21,9 @@ class ExamenVictorAracil{
 			Console.WriteLine("3 - Mostrar matriz");
 			Console.WriteLine("4 - Sumar matrices");
 			Console.WriteLine("5 - Restar matrices");	
-			Console.WriteLine("6 - Multiplicar matrices");	
+			Console.WriteLine("6 - Multiplicar matrices");
+			Console.WriteLine("7 - Hacer la traspuesta");
+
 			
 			op = Console.ReadLine();
 			
@@ -34,145 +36,127 @@ class ExamenVictorAracil{
 				case "2":
 					RellenarMatrices(ref matrices); break;
 				case "3":
-					MostrarMatriz(matrices); break;
+					MostrarMatriz(matrices, "+"); break;
 				case "4":
-					SumarMatrices(matrices); break;
+					SumarMatrices(matrices);
+					MostrarMatriz(matrices, "+"); break;
 				case "5":
-					RestarMatrices(matrices); break;
+					RestarMatrices(matrices);
+					MostrarMatriz(matrices, "-"); break;
 				case "6":
-					MultiplicarMatrices(matrices); break;
+					MultiplicarMatrices(matrices);
+					MostrarMatriz(matrices, "X"); break;
+				case "7":
+					Traspuesta(matrices); break;
 				default: Console.WriteLine("Opción incorrecta"); break;
 			}
 		}while (op != "0");
 
 
     }
+    
 	public static void DimensionarMatrices(ref structMatrices m, int dim){
-	
+		//Define las dimensiones de la matriz segun lo indicado por el usuario
 		m.m1 = new int [dim,dim];
 		m.m2 = new int [dim,dim];
 		m.mr = new int [dim,dim];
 	}
+	
 	public static void RellenarMatrices(ref structMatrices m){
-		Random R = new Random();
+		Random R = new Random();//Rellena automaticamente las matrices con numeros del 0 al 9 usando Random
 		for (int i = 0; i < m.m1.GetLength(0); i++){
 			for (int j = 0; j < m.m1.GetLength(1); j++){
 				m.m1[i,j] = R.Next(0, 9);
 				m.m2[i,j] = R.Next(0, 9);
 			}
 		}
-	}	
-	public static void MostrarMatriz(structMatrices m){
+	}
+		
+	public static void MostrarMatriz(structMatrices m, string operador){
+		//Muestra de manera grafica las matrices
 		for (int i = 0; i < m.m1.GetLength(0); i++){
-			Console.Write("|");
+			Console.Write("|  ");
 			for (int j = 0; j < m.m1.GetLength(1); j++){	
-				Console.Write(m.m1[i,j] + " ");	
+				Console.Write(m.m1[i,j] + "   ");	
 			}
 			Console.WriteLine("|");
 		}
-	Console.ReadKey();
-	Console.WriteLine("Presiona ENTER para continuar...");
-    Console.ReadLine();
-	}
-	public static void SumarMatrices(structMatrices m){
+		
+		Console.WriteLine(operador);
+		
+		for (int i = 0; i < m.m2.GetLength(0); i++){
+			Console.Write("|  ");
+			for (int j = 0; j < m.m2.GetLength(1); j++){	
+				Console.Write(m.m2[i,j] + "   ");	
+			}
+			Console.WriteLine("|");
+		}
+		
+		Console.WriteLine("=");
+		
 		for (int i = 0; i < m.mr.GetLength(0); i++){
+			Console.Write("|  ");
+			for (int j = 0; j < m.mr.GetLength(1); j++){	
+				Console.Write(m.mr[i,j] + "   ");	
+			}
+			Console.WriteLine("|");
+		}
+		Console.ReadKey();
+		Console.WriteLine("Presiona ENTER para continuar...");
+		Console.ReadLine();
+	}
+	
+	public static void SumarMatrices(structMatrices m){
+		for (int i = 0; i < m.mr.GetLength(0); i++){//Realiza la suma
 			for (int j = 0; j < m.mr.GetLength(1); j++){
 				m.mr[i,j] = m.m1[i,j] + m.m2[i,j];
 			}
 		}
-		
-		for (int i = 0; i < m.m1.GetLength(0); i++){
-			Console.Write("|  ");
-			for (int j = 0; j < m.m1.GetLength(1); j++){	
-				Console.Write(m.m1[i,j] + "   ");	
-			}
-			Console.WriteLine("|");
-		}
-		
-		Console.WriteLine("+");
-		
-		for (int i = 0; i < m.m2.GetLength(0); i++){
-			Console.Write("|  ");
-			for (int j = 0; j < m.m2.GetLength(1); j++){	
-				Console.Write(m.m2[i,j] + "   ");	
-			}
-			Console.WriteLine("|");
-		}
-		
-		Console.WriteLine("=");
-		
-		for (int i = 0; i < m.mr.GetLength(0); i++){
-			Console.Write("|  ");
-			for (int j = 0; j < m.mr.GetLength(1); j++){	
-				Console.Write(m.mr[i,j] + "   ");	
-			}
-			Console.WriteLine("|");
-		}
-		Console.ReadKey();
-		Console.WriteLine("Presiona ENTER para continuar...");
-		Console.ReadLine();
 	}
+	
 	public static void RestarMatrices(structMatrices m){
-		for (int i = 0; i < m.mr.GetLength(0); i++){
+		for (int i = 0; i < m.mr.GetLength(0); i++){//Realiza la resta
 			for (int j = 0; j < m.mr.GetLength(1); j++){
 				m.mr[i,j] = m.m1[i,j] - m.m2[i,j];
 			}
 		}
-		
-		for (int i = 0; i < m.m1.GetLength(0); i++){
-			Console.Write("|  ");
-			for (int j = 0; j < m.m1.GetLength(1); j++){	
-				Console.Write(m.m1[i,j] + "   ");	
-			}
-			Console.WriteLine("|");
-		}
-		
-		Console.WriteLine("+");
-		
-		for (int i = 0; i < m.m2.GetLength(0); i++){
-			Console.Write("|  ");
-			for (int j = 0; j < m.m2.GetLength(1); j++){	
-				Console.Write(m.m2[i,j] + "   ");	
-			}
-			Console.WriteLine("|");
-		}
-		
-		Console.WriteLine("=");
-		
-		for (int i = 0; i < m.mr.GetLength(0); i++){
-			Console.Write("|  ");
-			for (int j = 0; j < m.mr.GetLength(1); j++){	
-				Console.Write(m.mr[i,j] + "   ");	
-			}
-			Console.WriteLine("|");
-		}
-		Console.ReadKey();
-		Console.WriteLine("Presiona ENTER para continuar...");
-		Console.ReadLine();
 	}
+	
 	public static void MultiplicarMatrices(structMatrices m){
-		for (int i = 0; i < m.mr.GetLength(0); i++){
+		
+		for (int i = 0; i < m.mr.GetLength(0); i++){//Reinicia la matriz resultado
 			for (int j = 0; j < m.mr.GetLength(1); j++){
-				for (int z = 1; z < m.mr.GetLength(1); z++){
-					m.mr[i,j] = (m.m1[i,j] * m.m2[i,z]) + (m.m1[i,z] * m.m2[z,j]);
+				m.mr[i,j] = 0;
+			}
+		}
+		
+		for (int i = 0; i < m.mr.GetLength(0); i++){//realiza la multiplicacion
+			for (int j = 0; j < m.mr.GetLength(1); j++){
+				for (int z = 0; z < m.mr.GetLength(1); z++){
+					m.mr[i,j] += m.m1[i,z] * m.m2[z,j]; 
 				}	
 			}
 		}
+	}
+	
+	public static void Traspuesta(structMatrices m){
+		
+		for (int i = 0; i < m.mr.GetLength(0); i++){//Reinicia la matriz resultado
+			for (int j = 0; j < m.mr.GetLength(1); j++){
+				m.mr[i,j] = 0;
+			}
+		}
+		
+		for (int i = 0; i < m.m1.GetLength(0); ++i) {//Cambia filas por columnas, haciendo la traspuesta
+			for (int j = 0; j < m.m1.GetLength(1); ++j) {
+				m.mr[i, j] = m.m1[j, i];
+			}
+		}
 		
 		for (int i = 0; i < m.m1.GetLength(0); i++){
 			Console.Write("|  ");
 			for (int j = 0; j < m.m1.GetLength(1); j++){	
 				Console.Write(m.m1[i,j] + "   ");	
-			}
-			Console.WriteLine("|");
-		}
-		
-		Console.WriteLine("+");
-		
-		for (int i = 0; i < m.m2.GetLength(0); i++){
-			Console.Write("|  ");
-			for (int j = 0; j < m.m2.GetLength(1); j++){	
-				Console.Write(m.m2[i,j] + "   ");	
 			}
 			Console.WriteLine("|");
 		}
@@ -188,6 +172,6 @@ class ExamenVictorAracil{
 		}
 		Console.ReadKey();
 		Console.WriteLine("Presiona ENTER para continuar...");
-		Console.ReadLine();
-	}		
+		Console.ReadLine();		
+	}
 }
